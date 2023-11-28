540ef4371a9c320133ffe7c431272c594a47e3be
matshidisolp <mpitswane@gmail.com>
2023-11-28 10:10:22 +0700

    linked-list task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,23 +2,21 @@
 
 /**
  * cycle_check - check if linked list has a cycle
- * @list: linked list to be checked
+ * @head: linked list to be checked
  * Description: Uses Floyd's cycle finding algorithm to check for a cycle
  * Return: 1 if true_cycle, 0 if false_no cycle
  */
-int cycle_check(listint_t *list)
+int cycle_check(listint_t *head)
 {
-	listint_t *slow = list;
-	listint_t *fast = list;
+	listint_t *slow = head;
+	listint_t *fast = head;
 
-	if (list == NULL)
-		return (0);
 	while (fast && fast->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
 		if (slow == fast)
-			return(1);
+			return (1);
 	}
 	return (0);
 }
