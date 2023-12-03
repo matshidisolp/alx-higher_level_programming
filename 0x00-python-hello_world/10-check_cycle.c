3b0afca6cb896632d8bcbc9afbea2fe437ee122e
matshidisolp <mpitswane@gmail.com>
2023-12-03 10:48:53 +0700

    betty documentation

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,16 +8,17 @@
  */
 int cycle_check(listint_t *list)
 {
+	listint_t *slow = list, *fast = list;
+
 	if (list == NULL)
 		return (0);
-	listint_t *slow = list, *fast = list;
 
 	while (fast && fast->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
 
-		if (slow = fast)
+		if (slow == fast)
 			return (1);
 	}
 	return (0);
