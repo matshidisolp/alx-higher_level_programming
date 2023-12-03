e2c12f70f5bed66e28b8969be43a2c5b2e601750
matshidisolp <mpitswane@gmail.com>
2023-12-03 09:39:51 +0700

    betty documentation

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,20 +2,22 @@
 
 /**
  * cycle_check - check if linked list has a cycle
- * @head: linked list to be checked
+ * @list: linked list to be checked
  * Description: Uses Floyd's cycle finding algorithm to check for a cycle
  * Return: 1 if true_cycle, 0 if false_no cycle
  */
-int cycle_check(listint_t *head)
+int cycle_check(listint_t *list)
 {
-	listint_t *slow = head;
-	listint_t *fast = head;
+	if (list == NULL)
+		return (0);
+	listint_t *slow = list, *fast = list;
 
 	while (fast && fast->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
-		if (slow == fast)
+
+		if (slow = fast)
 			return (1);
 	}
 	return (0);
