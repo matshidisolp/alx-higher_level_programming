c85f05dfc3aa3cf0c970b543eeaad7c681b97e5e
matshidisolp <mpitswane@gmail.com>
2023-12-03 09:17:46 +0700

    adding with to string

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,5 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
 str = str[39:66] + str[113:119] + str[:6]
-#str = str[39:67] + str[68:74] + str[68:71] + str[:6]
 print(str)
