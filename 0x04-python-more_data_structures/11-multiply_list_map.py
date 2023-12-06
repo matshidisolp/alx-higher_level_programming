04a7073d7226f9fefe0e74d8ceebea3194e476a3
matshidisolp <mpitswane@gmail.com>
2023-12-07 04:47:27 +0700

    Task 11 and 12 version 1

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,8 @@
+#def multiply_list_map(my_list=[], number=0):
+#return (list(map((lambda i: i * number), my_list)))
+#!/usr/bin/python3
+
+def multiply_list_map(my_list=None, number=0):
+    if my_list is None:
+        my_list = []
+    return list(map(lambda x: x * number, my_list))
