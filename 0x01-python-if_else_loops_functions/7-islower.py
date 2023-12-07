4ba5194f95f5d490a8dc3bc89fa396004d2f14cf
matshidisolp <mpitswane@gmail.com>
2023-12-08 06:52:48 +0700

    Task 7 corrections

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# 7-islower.py
-
 
 def islower(c):
     """Check for lowercase characters."""
@@ -8,11 +6,9 @@ def islower(c):
         return True
     else:
         return False
+
+
 # Call the function and print the result
 char_to_check = 'a'
 result = islower(char_to_check)
 print(result)
-#alternative script
-	def islower(c):
-    """Check for lowercase characters."""
-    return ord('a') <= ord(c) <= ord('z')
