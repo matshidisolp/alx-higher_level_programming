dfaf23fd8918c146a381d3cdd5772b990bd469b8
matshidisolp <mpitswane@gmail.com>
2024-05-23 09:11:06 +0700

    1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- Create user 'user_0d_1' if it does not already exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to 'user_0d_1' on all databases
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
