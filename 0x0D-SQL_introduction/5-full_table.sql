a754917298fb7269700508a45a6b9066d4989703
matshidisolp <mpitswane@gmail.com>
2024-06-06 09:10:45 +0700

    5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,7 +1,6 @@
---prints the full description of first_table from 
---the database hbtn_0c_0 in MySQL server
+-- prints the full description of first_table from 
+-- the database hbtn_0c_0 in MySQL server
 SELECT COLUMN_NAME, COLUMN_TYPE
 FROM information_schema.COLUMNS
 WHERE TABLE_SCHEMA = 'hbtn_0c_0'
 AND TABLE_NAME = 'first_table';
-
