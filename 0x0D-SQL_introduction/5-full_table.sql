7d4c08ee2948f2b8850275a98a338c075a075448
matshidisolp <mpitswane@gmail.com>
2024-06-06 11:05:16 +0700

    5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,12 +1,10 @@
--- Prints the full description of the table 
--- first_table from the database hbtn_0c_0
-SELECT CONCAT(
-    'first_table',
-    'CREATE TABLE `first_table` (',
-    '`id` int NOT NULL AUTO_INCREMENT,',
-    '`name` varchar(128) DEFAULT NULL,',
-    '`c` char(1) DEFAULT NULL,',
-    '`created_at` date DEFAULT NULL,',
-    'PRIMARY KEY (`id`)',
-    ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci'
-) AS full_description;
+-- Prints the full description of first_table from
+-- the database hbtn_0c_0 in MySQL server
+SELECT
+    COLUMN_NAME,
+    COLUMN_TYPE
+FROM
+    information_schema.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'hbtn_0c_0'
+    AND TABLE_NAME = 'first_table';
