0fb56b79751b5c500e1bfac6fd46528cafd5bba6
matshidisolp <mpitswane@gmail.com>
2024-06-06 09:34:02 +0700

    10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
---lists all records of second_table of the db hbtn_0c_0 in MySQL server
---records ordered by score(top first)
+-- lists all records of second_table of the db hbtn_0c_0 in MySQL server
+-- records ordered by score(top first)
 SELECT score, name 
 FROM second_table 
 ORDER BY score DESC;
