3fb7000e50d6f6e30cf9342aac9ffb1d9ae3a6a9
matshidisolp <mpitswane@gmail.com>
2024-06-06 09:14:46 +0700

    6-list_values.sql

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,4 +1,3 @@
---lists all rows of the first_table from 
---the database hbtn_0c_0 in MySQL server
+-- lists all rows of the first_table from 
+-- the database hbtn_0c_0 in MySQL server
 SELECT * FROM first_table;
-
