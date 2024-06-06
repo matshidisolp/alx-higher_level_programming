08b0ad7ed3c880bc8d5bc49dc78b380bcd284a34
matshidisolp <mpitswane@gmail.com>
2024-06-06 09:35:34 +0700

    11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,7 +1,6 @@
---lists all records with a score >= 10 in second_table 
---of the database hbtn_0c_0 in MySQL server.
+-- lists all records with a score >= 10 in second_table
+-- of the database hbtn_0c_0 in MySQL server.
 SELECT score, name
 FROM second_table
 WHERE score >= 10
 ORDER BY score DESC;
-
