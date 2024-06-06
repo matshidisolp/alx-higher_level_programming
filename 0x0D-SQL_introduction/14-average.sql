2b119f4e4d450158366276e7e33897f6813c05b9
matshidisolp <mpitswane@gmail.com>
2024-06-06 10:41:12 +0700

    14-average.sql

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
---computes the score average of all records 
--- in the table second_table hbtn_0c_0 database
+-- Computes the average score of all records 
+-- in the table second_table
 SELECT AVG(score) AS average
 FROM second_table;
