d3d7001d6be28ba74ee2512451ef435e5d694b30
matshidisolp <mpitswane@gmail.com>
2024-06-06 09:38:31 +0700

    13-change_class.sql

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,4 +1,4 @@
---removes all records with a score <= 5 in the second_table 
---of hbtn_0c_0 in your MySQL server
+-- removes all records with a score <= 5 in the second_table 
+-- of hbtn_0c_0 in your MySQL server
 DELETE FROM second_table
 WHERE score <= 5;
