2588eda56528929b2168ecbe07a8343c4bdb394c
matshidisolp <mpitswane@gmail.com>
2024-06-19 19:46:58 +0700

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-
-// prints specific lines
+/* print specific lines using console */
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
