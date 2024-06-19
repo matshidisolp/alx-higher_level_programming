eafc78ba4cfb0a6da0ad06a32389fa635797dae4
matshidisolp <mpitswane@gmail.com>
2024-06-19 22:07:29 +0700

    7-multi_c.js

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* prints "C is fun" x times where x is first argument" */
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('C is fun');
+    i++;
+  }
+}
