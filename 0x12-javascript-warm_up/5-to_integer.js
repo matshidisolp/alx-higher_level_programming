de2cc19927e5c95307b749fedbb9b63752398db4
matshidisolp <mpitswane@gmail.com>
2024-06-19 21:46:33 +0700

    5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* prints the first argument as an integer */
+if (isNaN(process.argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
+}
