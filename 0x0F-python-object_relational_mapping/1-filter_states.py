4a92beaf74036dccd351dd5acd9985cfaed07f1c
matshidisolp <mpitswane@gmail.com>
2024-06-25 13:02:39 +0700

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python3i
 
 """
 Module to connect to a MySQL database and retrieve all from states table.
@@ -15,12 +15,27 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+
+    query = (
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' "
+        "ORDER BY id ASC"
+    )
+    cur.execute(query)
+
     rows = cur.fetchall()
+
     for row in rows:
         print(row)
+
     cur.close()
     db.close()
