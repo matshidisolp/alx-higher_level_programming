ab0f7be420349a7431568a2699aa062ae03cd2a9
matshidisolp <mpitswane@gmail.com>
2024-07-04 19:35:21 +0700

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-#sends a request to given URL
-#displays the size of the body of the response
-
-curl -s "$1" | grep Content-Length | cut -f 2 -d ':'
+# get the response content length
+curl -Is "$1" | grep Content-Length | cut -f 2 -d ':'
