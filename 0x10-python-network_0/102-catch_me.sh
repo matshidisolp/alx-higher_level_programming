3af80e3e5d4a748586f1e41dc02463f49ef6a389
matshidisolp <mpitswane@gmail.com>
2024-07-08 08:52:34 +0700

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# sends a request to a request to 0.0.0.0:5000/catch_me that causes
-# server to respond with You got me!, in the body of the response
-curl -sL -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
+# sends a request to given URL and cause a specific response
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:HolbertonSchool"
