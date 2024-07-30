a4abfdcd01bb2213d33a92dea4b53ac5cbd5980f
matshidisolp <mpitswane@gmail.com>
2024-07-30 19:05:44 +0700

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,23 +4,20 @@
 const request = require('request');
 
 const apiUrl = process.argv[2];
-
 const characterId = '18';
 
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
-  } else {
+    return;
+  }
+
+  try {
     const data = JSON.parse(body);
     const films = data.results;
-    let count = 0;
-
-    films.forEach(film => {
-      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-        count++;
-      }
-    });
-
+    const count = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length;
     console.log(count);
+  } catch (err) {
+    console.error('Error parsing JSON:', err);
   }
 });
