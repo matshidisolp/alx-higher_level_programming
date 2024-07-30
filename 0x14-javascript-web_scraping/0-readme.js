1a4e20cc345b4269a131e070c40ca87e97550f3f
matshidisolp <mpitswane@gmail.com>
2024-07-30 18:57:39 +0700

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,12 +2,13 @@
 // Reads and prints content of a file
 
 const fs = require('fs');
+
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(data);
-    }
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
