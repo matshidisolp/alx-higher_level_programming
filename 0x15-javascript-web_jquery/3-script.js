d4eaf1af4013ae458a90a1baebf43c65ddecfcdb
matshidisolp <mpitswane@gmail.com>
2024-08-07 15:47:54 +0700

    3-script.js

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,6 @@
+// Add a red class to the header tag and turn it 
+// red when the div#red_header tag is clicked using API JQuery
+
+$('div#red_header').click(function () {
+    $('header').addClass('red');
+  });
